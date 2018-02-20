b85c0d281f389f33bc7efc3a5989d1872ff9f1eb
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-19 17:04:13 -0800

    can load tweets from back end, tokens hidden

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'isomorphic-fetch';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      tweets: [],
+    }
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    return fetch('http://localhost:3001/api/tweets/load_tweets/', {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        search: this.state.search
+      })
+    }).then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      this.setState({
+        tweets: responseJSON
+      })
+    })
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
   render() {
+    const tweets = this.state.tweets.map((tweet, index) => {
+      return (
+        <li key={index}>{tweet.full_text}</li>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>Search Tweets </h3>
+        <form onSubmit={this.handleOnSubmit}>
+          <input onChange={this.handleOnChange} type="text" value={this.state.search} />
+          <input type="submit" value="search" />
+        </form>
+        <h3>Tweet results</h3>
+        <ol>
+          {tweets}
+        </ol>
       </div>
     );
   }
