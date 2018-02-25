0749ca951dcdca3c2a17d5ecdf36a93f35cc917a
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-24 21:51:45 -0800

    proper create posting can confirm is made

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -5,6 +5,40 @@ export const updateSearch = (searchKeyWords) => {
 }
 
 export const storeSearch = (searchKeyWords) => {
-  // debugger;
-  return {type: 'STORE_SEARCH', searchKeyWords}
+  return function(dispatch) {
+    return fetch('http://localhost:3001/api/searches', {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        search: searchKeyWords
+      })
+    })
+  }
+  // return {type: 'STORE_SEARCH', searchKeyWords}
 }
+
+// export const storeSearch = (searchKeyWords) => {
+//   debugger;
+//   return function(dispatch) {
+//     debugger;
+//     // dispatch({type: LOADING_TWEETS})
+//     return fetch('http://localhost:3001/api/searches', {
+//       method: "post",
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         search: searchKeyWords
+//       })
+//     })
+//       .then(response => response.json())
+//       .then(search => {
+//         debugger;
+//         dispatch({type: 'STORE_SEARCH', searchKeyWords: search.keywords })
+//       })
+//   }
+// }
