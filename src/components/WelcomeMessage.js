09e5a73b02cb36889a908f728895848046e22ba1
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-25 16:37:37 -0800

    sort of silly welcomemessage component set up via Route

---
+++ src/components/WelcomeMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeMessage = () => {
+  return (
+    <h3>
+      Welcome to Twogle!
+    </h3>
+  )
+}
+
+export default WelcomeMessage;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import './App.css';
 import 'isomorphic-fetch';
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import WelcomeMessage from '../components/WelcomeMessage'
 import SearchBoxContainer from './SearchBoxContainer'
 import HeadlineList from '../components/HeadlineList'
 import TweetList from '../components/TweetList'
@@ -19,48 +20,50 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Twogle</h1>
+            <Route exact path="/" component={WelcomeMessage}/>
+          </header>
 
-        </header>
-
-        <div className="App-body">
-          <div className="flex-heading">
-            <SearchBoxContainer
-              fetchHeadlines={this.props.fetchHeadlines}
-              deleteTweets={this.props.deleteTweets}
-              fetchTweets={this.props.fetchTweets}
-            />
-          </div>
-          <br />
-          <div id="flex-body">
-            <div>
-              {this.props.headlines.results === false &&
-                <ZeroResults
-                  media="NewsAPI"
-                  searchedTerms={this.props.headlines.submittedSearch}
-                />
-              }
-              {this.props.headlines.results.length > 0 &&
-                <HeadlineList headlines={this.props.headlines}/>
-              }
+          <div className="App-body">
+            <div className="flex-heading">
+              <SearchBoxContainer
+                fetchHeadlines={this.props.fetchHeadlines}
+                deleteTweets={this.props.deleteTweets}
+                fetchTweets={this.props.fetchTweets}
+              />
             </div>
-            <div>
-              {this.props.tweets.results === false &&
-                <ZeroResults
-                  media="Twitter"
-                  searchedTerms={this.props.tweets.submittedSearch}
-                />
-              }
-              {this.props.tweets.results.length > 0 &&
-                <TweetList tweets={this.props.tweets}/>
-              }
-            </div>
+            <br />
+            <div id="flex-body">
+              <div>
+                {this.props.headlines.results === false &&
+                  <ZeroResults
+                    media="NewsAPI"
+                    searchedTerms={this.props.headlines.submittedSearch}
+                  />
+                }
+                {this.props.headlines.results.length > 0 &&
+                  <HeadlineList headlines={this.props.headlines}/>
+                }
+              </div>
+              <div>
+                {this.props.tweets.results === false &&
+                  <ZeroResults
+                    media="Twitter"
+                    searchedTerms={this.props.tweets.submittedSearch}
+                  />
+                }
+                {this.props.tweets.results.length > 0 &&
+                  <TweetList tweets={this.props.tweets}/>
+                }
+              </div>
 
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
