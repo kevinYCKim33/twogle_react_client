0740922be2a6ca21f73d9871edfcf2fcf3e2ac31
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-26 16:49:46 -0800

    history grid set up

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,40 +1,37 @@
 import React from 'react';
-import { PageHeader, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { NavLink, Link } from 'react-router-dom';
+// import { PageHeader, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+// import { NavLink, Link } from 'react-router-dom';
+import {Row, Col} from 'react-bootstrap'
 
 const HistoryList = (props) => {
-  const searches = props.search.prevSearches
-  let searchHistory;
-  if (searches.length > 0) {
-    searchHistory = searches.map(search => {
-      return (
-        <div>
-          <Link to={`/searches/${search.search_url}`}>
-            {search.keywords}
-          </Link>
-        </div>
-      )
-    })
-  }
+  // const searches = props.search.prevSearches
+  // let searchHistory;
+  // if (searches.length > 0) {
+  //   searchHistory = searches.map(search => {
+  //     return (
+  //       <div>
+  //         <Link to={`/searches/${search.search_url}`}>
+  //           {search.keywords}
+  //         </Link>
+  //       </div>
+  //     )
+  //   })
+  // }
   return (
-    <div className="History-Container">
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            Search History
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Clear History
-          </NavItem>
-        </Nav>
-      </Navbar>
-      <div className="History-Body">
-        <div className="list-history">
-          {searchHistory}
-        </div>
-      </div>
+    <div id="History">
+      <header className="History-header">
+        <div id="history-title">History</div>
+      </header>
+
+      <Row className="show-grid">
+        <Col xs={5} md={3}>
+          <code>&lt;{'Col xs={5} md={3}'} /&gt;</code>
+        </Col>
+        <Col xs={13} md={9}>
+          <code>&lt;{'Col xs={13} md={9}'} /&gt;</code>
+        </Col>
+      </Row>
+
     </div>
   )
 }
