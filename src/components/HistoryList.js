bdc1e9a288bda94cedb729da9f63652e7b40bb7e
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-26 21:33:59 -0800

    clear history link changed to button that looks like a link

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { PageHeader, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
-import {Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Button, Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap'
 import { exactDate } from '../helpers/timeConverter.js'
 
 const HistoryList = (props) => {
@@ -26,13 +26,13 @@ const HistoryList = (props) => {
       <br />
       <div id="history-column-container">
         <Col xs={5} md={3}>
-          <Link
-            to="#"
+          <Button
+            bsStyle="link"
             onClick={() => props.clearHistory()}
             style={{textDecoration: "none"}}
           >
             <b>Clear Search History</b>
-          </Link>
+          </Button>
         </Col>
         {searches.length > 0 &&
           <Col xs={13} md={9}>
