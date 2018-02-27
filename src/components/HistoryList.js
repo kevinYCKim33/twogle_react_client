64661e64c29b65fea5398c4d9f748aee2dd20ce4
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-26 18:13:25 -0800

    timeConverter imported successfully

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,22 +1,24 @@
 import React from 'react';
 // import { PageHeader, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 // import { NavLink, Link } from 'react-router-dom';
-import {Row, Col} from 'react-bootstrap'
+import {Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap'
+import { exactDate } from '../helpers/timeConverter.js'
 
 const HistoryList = (props) => {
-  // const searches = props.search.prevSearches
-  // let searchHistory;
-  // if (searches.length > 0) {
-  //   searchHistory = searches.map(search => {
-  //     return (
-  //       <div>
-  //         <Link to={`/searches/${search.search_url}`}>
-  //           {search.keywords}
-  //         </Link>
-  //       </div>
-  //     )
-  //   })
-  // }
+  const searches = props.search.prevSearches
+  let searchHistory;
+  if (searches.length > 0) {
+    searchHistory = searches.map(search => {
+      // debugger;
+      const myDate = exactDate(search.updated_at)
+      debugger;
+      return (
+        <ListGroupItem>
+          {search.keywords}
+        </ListGroupItem>
+      )
+    })
+  }
   return (
     <div id="History">
       <header className="History-header">
@@ -25,32 +27,12 @@ const HistoryList = (props) => {
       <br />
       <div id="history-column-container">
         <Col xs={5} md={3}>
-          <b>Clear Browsing History</b>
+          <b>Clear Search History</b>
         </Col>
         <Col xs={13} md={9}>
-          <code>&lt;{'Col xs={13} md={9}'} /&gt;</code>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
-          <p> some text </p>
+          <ListGroup>
+            {searchHistory}
+          </ListGroup>
 
         </Col>
       </div>
---
+++ src/helpers/timeConverter.js
@@ -0,0 +1,34 @@
+const displayDoubleDigit = (number) => {
+  if (number < 10) {
+    return '0' + number.toString();
+  } else {
+    return number.toString();
+  }
+}
+
+const toStandardTime = (militaryTime) => {
+  let hour = Number(militaryTime.slice(0,2)); // 12
+  let minute = militaryTime.slice(3); // '51'
+  let extension = 'AM';
+
+  if (hour > 12) {
+    hour -= 12;
+    extension = 'PM';
+  } else if (hour === 12) {
+    extension = 'PM';
+  } else if (hour === 0) {
+    hour = 12;
+  }
+  let standardTime = displayDoubleDigit(hour) + ':' + minute + ' ' + extension;
+  return standardTime;
+}
+
+export const exactDate = (RailsTime) => {
+  // debugger;
+  const theExactDate = RailsTime
+  const date = new Date(theExactDate).toString().split(" ");
+  const monthDayYear = date.slice(1,3).join(" ") + ", " + date[3];
+  const time = date[4].split(":").slice(0,2).join(":");
+  const detailedTime = monthDayYear + " " + toStandardTime(time);
+  return detailedTime;
+}
