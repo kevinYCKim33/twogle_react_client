76af7cbfa757c392fa0feb921d426b41d39d2cd9
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-28 22:47:12 -0800

    twogle assesment pass

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Tweet = ({tweet}) => {
-  return (
-    <blockquote className="twitter-tweet" data-lang={tweet.lang}><p lang={tweet.lang} dir="ltr">{tweet.full_text}</p>&mdash; {tweet.user.name} (@{tweet.user.screen_name}) <a href={"https://twitter.com/" + tweet.user.screen_name + "/status/" + tweet.id_str + "?ref_src=twsrc%5Etfw"}>.</a></blockquote>
-  )
+class Tweet extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      likes: 0
+    }
+  }
+
+  handleButtonClick = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  callApi = () => {
+    console.log('a')
+    fetch('http://localhost:3001/api/searchesaj')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error(response.statusText)
+        }
+        console.log('b')
+        return (response.json())
+      }).then(searches => {
+        console.log('c',searches)
+      })
+      .catch(err => console.log('d', err))
+      console.log('e')
+
+      // a e b c + searchers
+      // a e d + err
+  }
+
+  render () {
+    return (
+      <div>
+        <blockquote className="twitter-tweet" data-lang={this.props.tweet.lang}><p lang={this.props.tweet.lang} dir="ltr">{this.props.tweet.full_text}</p>&mdash; {this.props.tweet.user.name} (@{this.props.tweet.user.screen_name}) <a href={"https://twitter.com/" + this.props.tweet.user.screen_name + "/status/" + this.props.tweet.id_str + "?ref_src=twsrc%5Etfw"}>.</a></blockquote>
+        <div>
+          <button onClick={this.handleButtonClick}> like</button>
+          <button onClick={this.callApi}> call api</button>
+          <b>{this.state.likes} likes!</b>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Tweet;
