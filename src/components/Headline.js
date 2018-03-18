f18590d078c517c13e563525ed21b81f2a02bb8d
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-03-18 16:47:55 -0700

    fixed flexbox on headlines

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -5,11 +5,14 @@ import {ListGroupItem} from 'react-bootstrap';
 const Headline = ({headline}) => {
   return (
     <ListGroupItem href={headline.url} className="headline-panel">
-      <img style={{width: "75px"}} src={headline.urlToImage} alt={headline.title} />
-      <br />
-      <b>{headline.title}</b>
-      <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
-      <div>{headline.description}</div>
+      <div className="flex-headline">
+        <img src={headline.urlToImage} alt={headline.title} />
+        <div>
+          <div style={{color: 'blue'}}>{headline.title}</div>
+          <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
+          <div>{headline.description}</div>
+        </div>
+      </div>
     </ListGroupItem>
   )
 }
