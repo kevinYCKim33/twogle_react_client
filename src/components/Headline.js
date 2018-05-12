16556034991b141c10c6bc0c4c91c8d201ccb297
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-05-12 05:08:53 -0700

    exact date showing up on headline panel

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -3,6 +3,7 @@ import {ListGroupItem} from 'react-bootstrap';
 import defaultNewsPic from '../Newspaper-Icon.jpg';
 import { connect } from 'react-redux'; // lets you connect to the redux store
 import { bindActionCreators } from 'redux'; // lets you link dispatch actions directly to props
+import { exactDate } from '../helpers/timeConverter.js';
 
 
 const Headline = ({headline, addDefaultSrc}) => {
@@ -18,7 +19,7 @@ const Headline = ({headline, addDefaultSrc}) => {
         <img onError={addDefaultSrc} src={headline.urlToImage} alt={headline.title} />
         <div>
           <div style={{color: 'blue'}}>{headline.title}</div>
-          <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
+          <div><span style={{color: 'green'}}>({headline.source.name})</span> - {exactDate(headline.publishedAt)} </div>
           <div>{headline.description}</div>
         </div>
       </div>
