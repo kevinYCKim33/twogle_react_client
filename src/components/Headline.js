b61ef7edaf9c453cfc94241788424ea4df64859d
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-05-12 04:20:56 -0700

    broken image links and null image links handled using class Headline

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import {ListGroupItem} from 'react-bootstrap';
+import defaultNewsPic from '../Newspaper-Icon.jpg';
 
 
-const Headline = ({headline}) => {
-  return (
-    <ListGroupItem href={headline.url} className="headline-panel">
-      <div className="flex-headline">
-        <img src={headline.urlToImage} alt={headline.title} />
-        <div>
-          <div style={{color: 'blue'}}>{headline.title}</div>
-          <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
-          <div>{headline.description}</div>
+class Headline extends React.Component {
+  //handle broken image links
+  addDefaultSrc = (ev) => {
+    ev.target.src = defaultNewsPic
+  };
+
+  render() {
+    const {headline} = this.props;
+    if (!headline.urlToImage) {
+      headline.urlToImage = defaultNewsPic; //handles null image links
+    }
+    return (
+      <ListGroupItem href={headline.url} className="headline-panel">
+        <div className="flex-headline">
+          <img onError={this.addDefaultSrc} src={headline.urlToImage} alt={headline.title} />
+          <div>
+            <div style={{color: 'blue'}}>{headline.title}</div>
+            <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
+            <div>{headline.description}</div>
+          </div>
         </div>
-      </div>
-    </ListGroupItem>
-  )
+      </ListGroupItem>
+    );
+  }
+
 }
+
+// const Headline = ({headline}) => {
+//   if (!headline.urlToImage) {
+//     headline.urlToImage = defaultNewsPic;
+//   }
+//
+//   return (
+//     <ListGroupItem href={headline.url} className="headline-panel">
+//       <div className="flex-headline">
+//         <img onError={this.addDefaultSrc} src={headline.urlToImage} alt={headline.title} />
+//         <div>
+//           <div style={{color: 'blue'}}>{headline.title}</div>
+//           <div><span style={{color: 'green'}}>({headline.source.name})</span> - {new Date(headline.publishedAt).toString()} </div>
+//           <div>{headline.description}</div>
+//         </div>
+//       </div>
+//     </ListGroupItem>
+//   )
+// }
 
 export default Headline;
